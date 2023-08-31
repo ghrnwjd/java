2004515c044c54e1531ed022b769f9cbcc43357c
ghrnwjd <hoyoung.wjd@gmail.com>
2023-08-31 18:29:41 +0900

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    int yearsOfExpr;
+    String tech;
+
+    String name;
+
+    public Employee(int yearsOfExpr, String tech, String name) {
+        this.yearsOfExpr = yearsOfExpr;
+        this.tech = tech;
+        this.name = name;
+    }
+}
